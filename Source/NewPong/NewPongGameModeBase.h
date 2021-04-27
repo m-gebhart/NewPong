93dab8560452cb622612e7155403249e3479cbba
Michael Gebhart <michael1998gebhart@gmail.com>
2021-04-27 13:15:35 +0200

    Initial commit

---
+++ Source/NewPong/NewPongGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NewPongGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWPONG_API ANewPongGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
